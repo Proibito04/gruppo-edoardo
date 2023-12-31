a24496f221ff3703c61c0d7a795829f529f7aafb
edoardo <edoardobalzano01@gmail.com>
2023-12-31 16:48:53 +0100

    Add Astro Icon integration and update LanguagePicker component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import cloudflare from "@astrojs/cloudflare";
+import icon from "astro-icon";
 
 // https://astro.build/config
-
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
   output: "hybrid",
   adapter: cloudflare(),
   i18n: {
     defaultLocale: "it",
-    locales: ["it", "en"],
-  },
+    locales: ["it", "en"]
+  }
 });
---
+++ src/componenti/LanguagePicker.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let selectedLang = "it"; // La lingua selezionata di default
+  import { language } from "../i18n/ui";
+  function switchLanguage(event) {
+    const lang = event.target.value;
+    if (lang === "it") {
+      // Se la lingua selezionata è l'italiano, vai alla root del sito
+      window.location.href = "/";
+    } else {
+      window.location.href = `/${lang}/`;
+    }
+  }
+</script>
+
+<select bind:value={selectedLang} on:change={switchLanguage}>
+  {#each Object.entries(language) as [lang, label]}
+    <option value={lang}>
+      {label}
+    </option>
+  {/each}
+</select>
